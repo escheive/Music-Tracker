b42fc8da3324ced7ad9f74cf7fab3bf01cbcf911
escheive <scheiveerik@gmail.com>
2024-04-24 18:26:05 -0700

    added logic to check for an accesstoken and fetch profileData if there is one

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -10,18 +10,36 @@ const redirectUri: string = import.meta.env.VITE_REDIRECT_URI;
 
 export default function Root() {
   const { accessToken, storeAccessToken, refreshToken, storeRefreshToken } = useAuthContext();
+  const { profileData, storeProfileData } = useUserContext();
 
+  // Checks for tokens and stores them if not stored yet
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const accessToken = params.get('access_token');
     const refreshToken = params.get('refresh_token');
 
+    if (accessToken && !profileData) {
+      const fetchProfile = async () => {
+        const profileOptions = {
+          method: 'get',
+          url: 'https://api.spotify.com/v1/me',
+          headers: {
+            'Authorization': 'Bearer ' + accessToken,
+          },
+        };
+  
+        const profileResponse = await axios(profileOptions);
+        storeProfileData(profileResponse.data);
+      }
+
+      fetchProfile();
+    }
+
     storeAccessToken(accessToken);
     storeRefreshToken(refreshToken);
 
-  }, [window.location.search]);
+  }, [accessToken]);
 
-  
 
   const handleLogin = async () => {
     const state = generateRandomString(16);
@@ -39,6 +57,20 @@ export default function Root() {
         <h1>Music Tracker</h1>
         <p>Access Token: {accessToken}</p>
         <p>Refresh Token: {refreshToken}</p>
+        {profileData ? (
+          <>
+            <p>Email: {profileData.email}</p>
+            <p>Name: {profileData.display_name}</p>
+            <p>Country: {profileData.country}</p>
+            <p>HREF: {profileData.href}</p>
+            <p>ID: {profileData.id}</p>
+            <p>Product: {profileData.product}</p>
+            <p>Type: {profileData.type}</p>
+            <p>Uri: {profileData.uri}</p>
+            <p>Followers: {profileData.followers.total}</p>
+            <p>External Urls: {profileData.external_urls.spotify}</p>
+          </>
+        ) : null}
         <button onClick={handleLogin}>Login</button>
       </div>
     </>
