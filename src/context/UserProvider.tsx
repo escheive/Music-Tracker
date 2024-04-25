a624c57dd16d5559e715e84ca44238087b42d725
escheive <scheiveerik@gmail.com>
2024-04-24 18:44:27 -0700

    added topItems to userContext to store a users topitems

--- src/context/UserProvider.tsx
+++ src/context/UserProvider.tsx
@@ -3,12 +3,16 @@ import React, { createContext, useContext, useState } from 'react';
 type UserContextType = {
   profileData: {} | null;
   storeProfileData: (profileData: {}) => void;
+  topItems: {} | null;
+  storeTopItems: (topItems: {}) => void;
 };
 
 
 const UserContext = createContext<UserContextType>({
   profileData: null,
   storeProfileData: (profileData: any) => {},
+  topItems: null,
+  storeTopItems: (topItems: any) => {},
 });
 
 export const useUserContext = () => {
@@ -19,15 +23,25 @@ export const useUserContext = () => {
   return context;
 };
 
-const UserContextProvider = ({ children }) => {
+const UserContextProvider = ({ children }: any) => {
   const [profileData, setProfileData] = useState(null);
+  const [topItems, setTopItems] = useState(null);
 
-  const storeProfileData = (profileData) => {
+  const storeProfileData = (profileData: any) => {
     setProfileData(profileData);
   };
 
+  const storeTopItems = (topItems: any) => {
+    setTopItems(topItems);
+  };
+
   return (
-    <UserContext.Provider value={{ profileData, storeProfileData }}>
+    <UserContext.Provider value={{ 
+      profileData, 
+      storeProfileData,
+      topItems,
+      storeTopItems
+    }}>
       {children}
     </UserContext.Provider>
   );
