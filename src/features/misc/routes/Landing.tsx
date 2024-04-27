4bcb0cafc003e53afe66a129416c588147098654
escheive <scheiveerik@gmail.com>
2024-04-26 20:10:35 -0700

    linking spotify redirects users to profile page instead of landing page

--- src/features/misc/routes/Landing.tsx
+++ src/features/misc/routes/Landing.tsx
@@ -50,7 +50,9 @@ export const Landing = () => {
       <Heading>Music Tracker</Heading>
       <Text m={2} textAlign='center'>Track your spotify account and connect with friends, all in one place using the Spotify API. No need to register an account, simply link your spotify account and see a breakdown of your entire account. To connect with others, register an account and start searching for your friends.</Text>
       <Box display='flex' justifyContent='center'>
-        <Button onClick={handleLinkSpotify} colorScheme='whatsapp' m={2}>Link Spotify</Button>
+        {!accessToken ? (
+          <Button onClick={handleLinkSpotify} colorScheme='whatsapp' m={2}>Link Spotify</Button>
+        ) : null}
         <Button onClick={handleLogin} colorScheme='blue' m={2}>Login</Button>
       </Box>
     </Box>
