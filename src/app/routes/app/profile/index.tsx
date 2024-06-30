644343c1784197727a18bc79671070c7cff84df9
escheive <scheiveerik@gmail.com>
2024-06-30 14:20:43 -0700

    restyled profile page to incude spotify logo

--- src/app/routes/app/profile/index.tsx
+++ src/app/routes/app/profile/index.tsx
@@ -4,12 +4,13 @@ import LineChart from "@/components/chart/LineChart";
 import { TopItemsList } from "@/components/list/TopItemsList";
 import { RecentlyPlayedList } from "@/components/list/RecentlyPlayedList";
 
-import { Box, Heading, Grid, GridItem, Link } from "@chakra-ui/react";
+import { Box, Heading, Grid, GridItem, Link, Flex, Image, Text } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { useRecentlyPlayedSongs, useSpotifyUser, useUsersTopItems } from "@api/spotify/spotify";
-import supabase from "@api/supabase/supabase";
 import { useAuthContext } from "@context/AuthProvider";
 import { useSupabaseUser } from "@api/supabase/fetch";
+import spotifyCMYKLogo from '@assets/spotify/logos/Spotify_Logo_CMYK_Green.png';
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 
 
 
@@ -22,8 +23,6 @@ export const ProfileRoute = () => {
   const [showRecentlyPlayed, setShowRecentlyPlayed] = useState(true);
   const { session } = useAuthContext();
   const { data: profile, error: profileError } = useSupabaseUser(session?.user.id);
-  console.log(profile)
-
 
   useEffect(() => {
     if (loggedOut) {
@@ -60,14 +59,27 @@ export const ProfileRoute = () => {
   return (
     <>
       <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' marginBlock='5%'>
-        <Heading>Music Tracker</Heading>
-        <Heading>{user.display_name}</Heading>
-        <p>Email: {user.email}</p>
-        <p>Country: {user.country}</p>
-        <p>Product: {user.product}</p>
-        <p>Type: {user.type}</p>
-        <p>Followers: {user.followers.total}</p>
-        <a href={user.external_urls.spotify} target="blank">Open on Spotify</a>
+        <Box alignItems='flex-start' width='100%' padding={3}>
+          <Text fontSize='3xl'>{profile?.username}</Text>
+            <Link href={user.external_urls.spotify} target="_blank" isExternal _hover={{ fontStyle: 'normal'}}>
+              <Flex alignItems='center'>
+                <Image
+                  src={spotifyCMYKLogo} 
+                  width={['80px']}
+                  height={['24px']}
+                  fallbackSrc='https://via.placeholder.com/150' 
+                />
+                <Text fontSize={['md', 'lg']}>{user.display_name}</Text>
+                <ExternalLinkIcon mx='2px' />
+              </Flex>
+            </Link>
+            <Text>Email: {user.email}</Text>
+            <Text>Country: {user.country}</Text>
+            <Text>Product: {user.product}</Text>
+            <Text>Type: {user.type}</Text>
+            <Text>Followers: {user.followers.total}</Text>
+            <a href={user.external_urls.spotify} target="blank">Open on Spotify</a>
+        </Box>
 
 
         {!recentlyPlayedSongsLoading ? (
