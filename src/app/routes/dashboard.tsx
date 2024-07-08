2d23ceed5740811862c24eae8cfbc7eda06cc3eb
escheive <scheiveerik@gmail.com>
2024-07-08 14:22:55 -0700

    modified workflow to publish on push to gh-pages branch

--- src/app/routes/dashboard.tsx
+++ src/app/routes/dashboard.tsx
@@ -1,192 +1,3 @@
-// import { useNavigate } from 'react-router';
-// import { 
-//   Box, 
-//   Heading, 
-//   Text, 
-//   FormControl,
-//   FormLabel,
-//   Textarea,
-//   Button, 
-//   Radio,
-//   RadioGroup,
-//   Stack,
-//   Modal,
-//   ModalOverlay,
-//   ModalContent,
-//   ModalHeader,
-//   ModalCloseButton,
-//   ModalFooter,
-//   ModalBody,
-//   Flex,
-//   Image,
-//   Link,
-// } from '@chakra-ui/react';
-
-// import { beginLogin } from '@/api/spotify/auth';
-// import { useRecentlyPlayedSongs, useSpotifyUser, useUsersTopItems } from '@api/spotify/spotify';
-// import { useAuthContext } from '@context/AuthProvider';
-// import { useSupabasePosts } from '@api/supabase/fetch';
-// import { useState } from 'react';
-// import { createPost } from '@api/supabase/insert';
-
-
-// export const DashboardRoute = () => {
-//   const navigate = useNavigate();
-//   const { user } = useSpotifyUser();
-//   const { session } = useAuthContext();
-//   const { data: posts, mutate: postsMutate, error: errorMutate } = useSupabasePosts();
-//   const { data: recentlyPlayedSongs } = useRecentlyPlayedSongs();
-//   const { data: topItems } = useUsersTopItems();
-//   const [draftedPost, setDraftedPost] = useState({
-//     user_id: session?.user.id,
-//     type: 'general',
-//     content: '',
-//     metadata: null
-//   });
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-//   const [selectedPost, setSelectedPost] = useState(null);
-
-//   const openModal = (post) => {
-//     setSelectedPost(post);
-//     setIsModalOpen(true);
-//   };
-
-//   const closeModal = () => {
-//     setSelectedPost(null);
-//     setIsModalOpen(false);
-//   };
-
-//   console.log(posts)
-
-//   const handlePostChange = (e) => {
-//     const { name, value } = typeof e === 'object' ? e.target : { name: 'type', value: e};
-//     setDraftedPost(prevState => ({
-//       ...prevState,
-//       [name]: value
-//     }));
-//   };
-
-//   const handlePostSubmit = () => {
-//     let metadata: any;
-//     if (draftedPost.type == 'recentlyPlayed') {
-//       metadata = recentlyPlayedSongs;
-//     } else if (draftedPost.type == 'topItems') {
-//       metadata = topItems;
-//     }
-
-//     createPost({
-//       ...draftedPost,
-//       metadata: metadata
-//     })
-//     setDraftedPost({
-//       user_id: session?.user.id,
-//       type: 'general',
-//       content: '',
-//       metadata: null
-//     }); // Reset text area after submission
-//   };
-
-//   return (
-//     <Box display='flex' flexDirection='column' alignItems='center' marginBlock='5%' >
-//       <Heading>Music Tracker for Spotify</Heading>
-//       <Box width='100%' maxW='600px' mx='auto' mt={4} p={4}>
-//         <FormControl id="post">
-//           <FormLabel>What's on your mind?</FormLabel>
-//           <Textarea
-//             name='content'
-//             value={draftedPost.content}
-//             onChange={handlePostChange}
-//             placeholder='Type your post here...'
-//             size='md'
-//             resize='vertical'
-//           />
-//         </FormControl>
-//         <FormLabel>Share what you're listening to!</FormLabel>
-//         <RadioGroup 
-//           name='type' 
-//           value={draftedPost.type}
-//           onChange={handlePostChange}
-//         >
-//           <Stack spacing={5} direction='row'>
-//             <Radio colorScheme='green' name='type' value='general'>
-//               No thanks
-//             </Radio>
-//             <Radio colorScheme='green' name='type' value='recentlyPlayed'>
-//               Recently Played Songs
-//             </Radio>
-//             <Radio colorScheme='green' value='topItems'>
-//               Top Tracks and Artists
-//             </Radio>
-//           </Stack>
-//         </RadioGroup>
-//         <Button
-//           mt={4}
-//           colorScheme='teal'
-//           onClick={handlePostSubmit}
-//         >
-//           Post
-//         </Button>
-//       </Box>
-//       <Box flexDirection='column' width='60%'>
-
-//         {posts?.map((post) => {
-//           return (
-//             <Box 
-//               key={post.id}
-//               p={4} 
-//               mb={6} 
-//               bg='white' 
-//               boxShadow='md'
-//               borderRadius='md'
-//             >
-//               <Text>{post.user.username}</Text>
-//               <Text fontSize="lg" mb={2} fontWeight="bold">{post.content}</Text>
-//               {post.type == 'recentlyPlayed' ? (
-//                 <>
-//                   <Text fontSize="md" fontWeight="semibold">Recently Played Songs:</Text>
-//                   {post?.metadata.slice(0, 3).map((song, index) => (
-//                     <Flex key={index} alignItems="center" mb={2}>
-//                       <Image 
-//                         src={song.album.images[0]?.url} 
-//                         alt={song.name} 
-//                         boxSize="40px" 
-//                         borderRadius="md" 
-//                         mr={3}
-//                       />
-//                       <Link href={song.external_urls.spotify} target='_blank'>
-//                         <Text>{song.name}</Text>
-//                       </Link>
-//                     </Flex>
-//                   ))}
-//                   <Button onClick={() => openModal(post)}>View More</Button>
-//                 </>
-//               ) : null}
-//             </Box>
-//           )
-//         })}
-//       </Box>
-//       {selectedPost && (
-//         <Modal isOpen={isModalOpen} onClose={closeModal}>
-//           <ModalOverlay />
-//           <ModalContent>
-//             <ModalHeader>Recently Played Songs</ModalHeader>
-//             <ModalCloseButton />
-//             <ModalBody>
-//               {selectedPost?.metadata.map((song, index) => (
-//                 <Text key={index}>{song.name}</Text>
-//               ))}
-//             </ModalBody>
-//             <ModalFooter>
-//               <Button colorScheme="blue" onClick={closeModal}>Close</Button>
-//             </ModalFooter>
-//           </ModalContent>
-//         </Modal>
-//       )}
-//     </Box>
-//   );
-// };
-
-import { useNavigate } from 'react-router';
 import { 
   Box, 
   Heading, 
@@ -210,10 +21,8 @@ import {
   Link,
 } from '@chakra-ui/react';
 
-import { beginLogin } from '@/api/spotify/auth';
-import { useRecentlyPlayedSongs, useSpotifyUser, useUsersTopItems } from '@api/spotify/spotify';
+import { useRecentlyPlayedSongs, useUsersTopItems } from '@api/spotify/spotify';
 import { useAuthContext } from '@context/AuthProvider';
-import { useSupabasePosts } from '@api/supabase/fetch';
 import { useEffect, useState } from 'react';
 import { createPost } from '@api/supabase/insert';
 import { useSupabasePostsInfinite } from "@api/supabase/fetch";
@@ -221,8 +30,6 @@ import { useInView } from 'react-intersection-observer';
 
 
 export const DashboardRoute = () => {
-  const navigate = useNavigate();
-  const { user } = useSpotifyUser();
   const { session } = useAuthContext();
   // const { data: posts, mutate: postsMutate, error: errorMutate } = useSupabasePosts();
   const { data: recentlyPlayedSongs } = useRecentlyPlayedSongs();
@@ -234,7 +41,7 @@ export const DashboardRoute = () => {
     metadata: null
   });
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedPost, setSelectedPost] = useState(null);
+  const [selectedPost, setSelectedPost] = useState<Record<string, any> | null>(null);
   const { data, size, setSize, error } = useSupabasePostsInfinite();
   const { ref, inView } = useInView();
   const [hasMore, setHasMore] = useState(true); // Flag to track if all posts are loaded
@@ -258,7 +65,7 @@ export const DashboardRoute = () => {
 
   if (error) return <Text>Error fetching posts</Text>;
 
-  const openModal = (post) => {
+  const openModal = (post: any) => {
     setSelectedPost(post);
     setIsModalOpen(true);
   };
@@ -268,10 +75,8 @@ export const DashboardRoute = () => {
     setIsModalOpen(false);
   };
 
-  console.log(posts)
-
-  const handlePostChange = (e) => {
-    const { name, value } = typeof e === 'object' ? e.target : { name: 'type', value: e};
+  const handlePostChange = (e: any) => {
+    const { name, value } = 'target' in e ? e.target : { name: 'type', value: e};
     setDraftedPost(prevState => ({
       ...prevState,
       [name]: value
@@ -370,7 +175,7 @@ export const DashboardRoute = () => {
                 {post.type == 'recentlyPlayed' ? (
                   <>
                     <Text fontSize='md' fontWeight='semibold'>Recently Played Songs:</Text>
-                    {post?.metadata.slice(0, 3).map((song, index) => (
+                    {post?.metadata.slice(0, 3).map((song: any, index: number) => (
                       <Flex key={index + song.id} alignItems='center' mb={2}>
                         <Image 
                           src={song.imageUrl} 
@@ -391,7 +196,7 @@ export const DashboardRoute = () => {
                     <Flex>
                       <Box flex='1' mr={4}>
                         <Text fontSize='lg' fontWeight='semibold' mb={2}>My Top Artists</Text>
-                        {post?.metadata.artists.map((artist, index) => (
+                        {post?.metadata.artists.map((artist: any, index: number) => (
                           <Flex key={index + artist.id} alignItems='center' mb={2}>
                             <Image 
                               src={artist.imageUrl} 
@@ -409,7 +214,7 @@ export const DashboardRoute = () => {
 
                       <Box flex='1'>
                         <Text fontSize='lg' fontWeight='semibold' mb={2}>My Top Tracks</Text>
-                        {post?.metadata.tracks.map((track, index) => (
+                        {post?.metadata.tracks.map((track: any, index: number) => (
                           <Flex key={index + track.id} alignItems='center' mb={2}>
                             <Image 
                               src={track.imageUrl} 
@@ -441,7 +246,7 @@ export const DashboardRoute = () => {
             <ModalHeader>Recently Played Songs</ModalHeader>
             <ModalCloseButton color='alternatePurple.300'/>
             <ModalBody>
-              {selectedPost?.metadata.map((song, index) => (
+              {selectedPost?.metadata.map((song: any, index: number) => (
                 <Flex key={index + song.id + 'modal'} alignItems='center' mb={2}>
                   <Image 
                     src={song.imageUrl} 
--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,113 +1,113 @@
-import { Link as ReactRouterLink } from 'react-router-dom';
-import * as z from 'zod';
+// import { Link as ReactRouterLink } from 'react-router-dom';
+// import * as z from 'zod';
 
-import { Button, Text, Input, Link as ChakraLink, Flex, Box, FormControl, FormLabel, useToast } from '@chakra-ui/react';
-import { useState } from 'react';
+// import { Button, Text, Input, Link as ChakraLink, Flex, Box, FormControl, FormLabel, useToast } from '@chakra-ui/react';
+// import { useState } from 'react';
 
-import supabase from '@api/supabase/supabase';
+// import supabase from '@api/supabase/supabase';
 
-const schema = z.object({
-  email: z.string().min(1, 'Required'),
-  password: z.string().min(1, 'Required'),
-});
+// const schema = z.object({
+//   email: z.string().min(1, 'Required'),
+//   password: z.string().min(1, 'Required'),
+// });
 
-type LoginValues = {
-  email: string;
-  password: string;
-};
+// type LoginValues = {
+//   email: string;
+//   password: string;
+// };
 
 
-export const LoginForm = () => {
-  const toast = useToast();
-  const [values, setValues] = useState<LoginValues>({
-    email: '',
-    password: '',
-  });
+// export const LoginForm = () => {
+//   const toast = useToast();
+//   const [values, setValues] = useState<LoginValues>({
+//     email: '',
+//     password: '',
+//   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setValues((prevValues) => ({
-      ...prevValues,
-      [name]: value,
-    }));
-  };
+//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const { name, value } = e.target;
+//     setValues((prevValues) => ({
+//       ...prevValues,
+//       [name]: value,
+//     }));
+//   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const { error } = await supabase.auth.signIn({ email, password });
+//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     const { error } = await supabase.auth.signIn({ email, password });
 
-    if (error) {
-      toast({
-        title: 'Error logging in',
-        description: error.message,
-        status: 'error',
-        duration: 5000,
-        isClosable: true,
-      });
-    } else {
-      toast({
-        title: 'Logged in successfully',
-        status: 'success',
-        duration: 5000,
-        isClosable: true,
-      });
-    }
+//     if (error) {
+//       toast({
+//         title: 'Error logging in',
+//         description: error.message,
+//         status: 'error',
+//         duration: 5000,
+//         isClosable: true,
+//       });
+//     } else {
+//       toast({
+//         title: 'Logged in successfully',
+//         status: 'success',
+//         duration: 5000,
+//         isClosable: true,
+//       });
+//     }
 
-    try {
-      // Validate form data using Zod schema
-      schema.parse(values);
+//     try {
+//       // Validate form data using Zod schema
+//       schema.parse(values);
 
-      // Call supabase API to register the user
-      const { data } = await supabase.auth.signInWithPassword({
-        email: values.email,
-        password: values.password,
-      })
+//       // Call supabase API to register the user
+//       const { data } = await supabase.auth.signInWithPassword({
+//         email: values.email,
+//         password: values.password,
+//       })
 
-      if (!data.session) {
-        console.log('Invalid email or password')
-      }
+//       if (!data.session) {
+//         console.log('Invalid email or password')
+//       }
 
-      if (data.session) {
+//       if (data.session) {
 
-      }
-    } catch (error: any) {
-      console.error('Error registering user:', error.message);
-    }
-  };
+//       }
+//     } catch (error: any) {
+//       console.error('Error registering user:', error.message);
+//     }
+//   };
 
-  return (
-    <Box p={4}>
-      <form onSubmit={handleSubmit}>
-        <FormControl id="email" isRequired>
-          <FormLabel>Email</FormLabel>
-          <Input
-            name='email'
-            type="email"
-            value={values.email}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl id="password" isRequired>
-          <FormLabel>Password</FormLabel>
-          <Input
-            name='password'
-            type="password"
-            value={values.password}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <Button mt={4} colorScheme="teal" type="submit">Register</Button>
-      </form>
-      <Flex paddingBlock='1'>
-        <Text>Don't have an account?</Text>
-        <ChakraLink 
-          as={ReactRouterLink} 
-          to="/auth/register" 
-          color='alternatePurple.400' paddingInline='1' _hover={{ color: 'alternatePurple.100' }}
-        >
-          Register
-        </ChakraLink>
-      </Flex>
-    </Box>
-  );
-};
+//   return (
+//     <Box p={4}>
+//       <form onSubmit={handleSubmit}>
+//         <FormControl id="email" isRequired>
+//           <FormLabel>Email</FormLabel>
+//           <Input
+//             name='email'
+//             type="email"
+//             value={values.email}
+//             onChange={handleChange}
+//           />
+//         </FormControl>
+//         <FormControl id="password" isRequired>
+//           <FormLabel>Password</FormLabel>
+//           <Input
+//             name='password'
+//             type="password"
+//             value={values.password}
+//             onChange={handleChange}
+//           />
+//         </FormControl>
+//         <Button mt={4} colorScheme="teal" type="submit">Register</Button>
+//       </form>
+//       <Flex paddingBlock='1'>
+//         <Text>Don't have an account?</Text>
+//         <ChakraLink 
+//           as={ReactRouterLink} 
+//           to="/auth/register" 
+//           color='alternatePurple.400' paddingInline='1' _hover={{ color: 'alternatePurple.100' }}
+//         >
+//           Register
+//         </ChakraLink>
+//       </Flex>
+//     </Box>
+//   );
+// };
