06e548ee3a691f4a30ae5e6a795d80d8dc731f47
escheive <scheiveerik@gmail.com>
2024-07-22 10:37:43 -0700

    more styling for find music route

--- src/app/routes/app/find/music.tsx
+++ src/app/routes/app/find/music.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Key, useState } from 'react';
 import {
   Box,
   Input,
@@ -9,7 +9,7 @@ import {
   Heading,
   Stack,
   Spinner,
-  Image,
+  Image as ChakraImage,
   Text,
   AlertIcon,
   Alert,
@@ -19,7 +19,7 @@ import {
   Link
 } from '@chakra-ui/react';
 import { useSpotifySearch } from '@api/spotify/spotify';
-import { Carousel } from '@components/carousel';
+import spotifyIcon from '@assets/spotify/icons/Spotify_Icon_RGB_White.png';
 
 export const FindMusicRoute = () => {
   const [query, setQuery] = useState('');
@@ -36,9 +36,11 @@ export const FindMusicRoute = () => {
     term: '',
     type: 'album,artist,track'
   });
-  const [expandAlbums, setExpandAlbums] = useState(false);
-  const [expandArtists, setExpandArtists] = useState(false);
-  const [expandTracks, setExpandTracks] = useState(false);
+  const [expandedCategories, setExpandedCategories] = useState({
+    album: true,
+    artist: true,
+    track: true
+  });
   const [expandedItem, setExpandedItem] = useState<string | null>(null);
 
   const toggleExpand = (id: string) => {
@@ -63,195 +65,199 @@ export const FindMusicRoute = () => {
     });
   };
 
+  const toggleExpandCategory = (category: 'album' | 'artist' | 'track') => {
+    setExpandedCategories(prevState => ({
+      ...prevState,
+      [category]: !prevState[category]
+    }));
+  };
+
   const breakpointColumns = useBreakpointValue({ base: 1, md: 2, lg: 3 });
 
-  const renderItems = (items, type) => {
-    
-    return items.map(item => (
-      <Box key={item.id} p={4} borderWidth={1} borderRadius="md" boxShadow="md">
-        <Box onClick={() => toggleExpand(item.id)} cursor="pointer">
-          <Image src={type !== 'track' ? item.images[0]?.url : item.album?.images[0]?.url} alt={item.name} boxSize="150px" objectFit="cover" />
-          <Text mt={2} fontWeight="bold">{item.name}</Text>
-          {type === 'album' && <Text>{item.artists.map(artist => artist.name).join(', ')}</Text>}
-          {type === 'artist' && <Text>Genres: {item.genres.join(', ')}</Text>}
-          {type === 'artist' && <Text>Followers: {item.followers.total}</Text>}
-          {type === 'track' && <Text>Artist: {item.artists.map(artist => artist.name).join(', ')}</Text>}
-          {type === 'track' && <Text>Album: {item.album.name}</Text>}
+  const renderItems = (items: Record<string, any>[], type: string) => {
+    return items.map((item: Record<string, any>) => (
+      <Box 
+        key={item.id} 
+        p={4} 
+        borderWidth={1} 
+        borderRadius='lg' 
+        boxShadow='lg' 
+        mb={4} 
+        bg='gray.800' 
+        color='white'
+        position='relative'
+      >
+        <Box 
+          onClick={() => toggleExpand(item.id)} 
+          cursor='pointer' 
+          display='flex' 
+          alignItems='center'
+        >
+          <ChakraImage
+            src={type !== 'track' ? item.images[0]?.url : item.album?.images[0]?.url}
+            alt={item.name}
+            boxSize='150px'
+            objectFit='cover'
+            borderRadius='md'
+          />
+          <Box ml={4}>
+            <Text fontSize='xl' fontWeight='bold'>{item.name}</Text>
+            {type === 'album' && <Text fontSize='md'>{item.artists.map(artist => artist.name).join(', ')}</Text>}
+            {type === 'artist' && <Text fontSize='md'>Genres: {item.genres.join(', ')}</Text>}
+            {type === 'artist' && <Text fontSize='md'>Followers: {item.followers.total}</Text>}
+            {type === 'track' && <Text fontSize='md'>Artist: {item.artists.map(artist => artist.name).join(', ')}</Text>}
+            {type === 'track' && <Text fontSize='md'>Album: {item.album.name}</Text>}
+          </Box>
         </Box>
         <Collapse in={expandedItem === item.id}>
-          {type === 'album' && (
-            <>
-              <Text>Release Date: {item.release_date}</Text>
-              <Text>Genre: {item.genres?.join(', ')}</Text>
-              <Text>Album Type {item.album_type}</Text>
-              <Text>Album Type {item.album_type}</Text>
-              <Text>Tracks: {item.total_tracks}</Text>
-              <Link href={item.external_urls.spotify} target='_blank'>Check out on Spotify</Link>
-            </>
-          )}
-          {type === 'artist' && (
-            <>
-              <Text>Popularity: {item.popularity}</Text>
-              <Text>Followers: {item.followers.total}</Text>
-              <Text>Genres: {item.genres.join(', ')}</Text>
-              <Link href={item.external_urls.spotify} target='_blank'>Check out on Spotify</Link>
-            </>
-          )}
-          {type === 'track' && (
-            <>
-              <Text>Duration: {Math.floor(item.duration_ms / 60000)}:{Math.floor((item.duration_ms % 60000) / 1000).toString().padStart(2, '0')}</Text>
-              <Text>Popularity: {item.popularity}</Text>
-              <Text>Album: {item.album.name}</Text>
-              <Link href={item.external_urls.spotify} target='_blank'>Check out on Spotify</Link>
-            </>
-          )}
+          <Box mt={4} p={4} borderWidth={1} borderRadius='md' boxShadow='md' bg='gray.700'>
+            {type === 'album' && (
+              <>
+                <Text>Release Date: {item.release_date}</Text>
+                <Text>Genre: {item.genres?.join(', ')}</Text>
+                <Text>Album Type: {item.album_type}</Text>
+                <Text>Tracks: {item.total_tracks}</Text>
+                <Link href={item.external_urls.spotify} target='_blank' color='teal.300'>Check out on Spotify</Link>
+              </>
+            )}
+            {type === 'artist' && (
+              <>
+                <Text>Popularity: {item.popularity}</Text>
+                <Text>Followers: {item.followers.total}</Text>
+                <Text>Genres: {item.genres.join(', ')}</Text>
+                <Link href={item.external_urls.spotify} target='_blank' color='teal.300'>Check out on Spotify</Link>
+              </>
+            )}
+            {type === 'track' && (
+              <>
+                <Text>Duration: {Math.floor(item.duration_ms / 60000)}:{Math.floor((item.duration_ms % 60000) / 1000).toString().padStart(2, '0')}</Text>
+                <Text>Popularity: {item.popularity}</Text>
+                <Text>Album: {item.album.name}</Text>
+                <Link href={item.external_urls.spotify} target='_blank' color='teal.300'>Check out on Spotify</Link>
+              </>
+            )}
+          </Box>
         </Collapse>
+        <ChakraImage 
+          src={spotifyIcon} 
+          alt='Spotify Icon' 
+          position='absolute' 
+          bottom={expandedItem === item.id ? 6 : 2} 
+          right={expandedItem === item.id ? 6 : 2} 
+          boxSize='40px' 
+        />
       </Box>
     ));
   };
 
-  // const renderItems = (items: Record<string, any>[], type: string) => {
-  //   return items.map(item => (
-  //     <Box key={item.id} p={4} borderWidth={1} borderRadius="md" boxShadow="md">
-  //       {type === 'album' && (
-  //         <Box>
-  //           <Image src={item.images[0]?.url} alt={item.name} boxSize="150px" objectFit="cover" />
-  //           <Text mt={2} fontWeight="bold">{item.name}</Text>
-  //           <Text>{item.artists.map((artist: Record<string, any>) => artist.name).join(', ')}</Text>
-  //           <Text>Release Date: {item.release_date}</Text>
-  //           <Text>Genre: {item.genres?.join(', ')}</Text>
-  //         </Box>
-  //       )}
-  //       {type === 'artist' && (
-  //         <Box>
-  //           <Image src={item.images[0]?.url} alt={item.name} boxSize="150px" objectFit="cover" />
-  //           <Text mt={2} fontWeight="bold">{item.name}</Text>
-  //           <Text>Genres: {item.genres.join(', ')}</Text>
-  //           <Text>Followers: {item.followers.total}</Text>
-  //         </Box>
-  //       )}
-  //       {type === 'track' && (
-  //         <Box>
-  //           <Image src={item.album.images[0]?.url} alt={item.name} boxSize="150px" objectFit="cover" />
-  //           <Text mt={2} fontWeight="bold">{item.name}</Text>
-  //           <Text>Artist: {item.artists.map((artist: Record<string, any>) => artist.name).join(', ')}</Text>
-  //           <Text>Album: {item.album.name}</Text>
-  //           <Text>Duration: {Math.floor(item.duration_ms / 60000)}:{Math.floor((item.duration_ms % 60000) / 1000).toString().padStart(2, '0')}</Text>
-  //         </Box>
-  //       )}
-  //     </Box>
-  //   ));
-  // };
-
   const { data, error, isLoading } = useSpotifySearch(search);
-  console.log(data)
 
   return (
-    <Box width="100%" maxW="800px" mx="auto" mt={4} p={4}>
+    <Box width='100%' mx='auto' mt={4} p={4}>
       <Heading mb={4}>Find Music</Heading>
       <form onSubmit={handleSearch}>
         <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
-          <FormControl id="query">
+          <FormControl id='query'>
             <FormLabel>Search Query</FormLabel>
             <Input 
               value={query}
               onChange={(e) => setQuery(e.target.value)}
-              placeholder="General search query"
+              placeholder='General search query'
             />
           </FormControl>
-          <FormControl id="type">
+          <FormControl id='type'>
             <FormLabel>Type</FormLabel>
             <Select 
               value={type}
               onChange={(e) => setType(e.target.value)}
             >
-              <option value="album,artist,track">All</option>
-              <option value="album">Album</option>
-              <option value="artist">Artist</option>
-              <option value="track">Track</option>
+              <option value='album,artist,track'>All</option>
+              <option value='album'>Album</option>
+              <option value='artist'>Artist</option>
+              <option value='track'>Track</option>
             </Select>
           </FormControl>
         </Stack>
         <Stack direction={{ base: 'column', md: 'row' }} spacing={4} mt={4}>
-          <FormControl id="artist">
+          <FormControl id='artist'>
             <FormLabel>Artist</FormLabel>
             <Input 
               value={artist}
               onChange={(e) => setArtist(e.target.value)}
-              placeholder="Artist name"
+              placeholder='Artist name'
             />
           </FormControl>
-          <FormControl id="album">
+          <FormControl id='album'>
             <FormLabel>Album</FormLabel>
             <Input 
               value={album}
               onChange={(e) => setAlbum(e.target.value)}
-              placeholder="Album name"
+              placeholder='Album name'
             />
           </FormControl>
-          <FormControl id="track">
+          <FormControl id='track'>
             <FormLabel>Track</FormLabel>
             <Input 
               value={track}
               onChange={(e) => setTrack(e.target.value)}
-              placeholder="Track name"
+              placeholder='Track name'
             />
           </FormControl>
         </Stack>
         <Stack direction={{ base: 'column', md: 'row' }} spacing={4} mt={4}>
-          <FormControl id="year">
+          <FormControl id='year'>
             <FormLabel>Year</FormLabel>
             <Input 
               value={year}
               onChange={(e) => setYear(e.target.value)}
-              placeholder="Year or range (e.g., 1955-1960)"
+              placeholder='Year or range (e.g., 1955-1960)'
             />
           </FormControl>
-          <FormControl id="genre">
+          <FormControl id='genre'>
             <FormLabel>Genre</FormLabel>
             <Input 
               value={genre}
               onChange={(e) => setGenre(e.target.value)}
-              placeholder="Genre"
+              placeholder='Genre'
             />
           </FormControl>
-          <FormControl id="isrc">
+          <FormControl id='isrc'>
             <FormLabel>ISRC</FormLabel>
             <Input 
               value={isrc}
               onChange={(e) => setIsrc(e.target.value)}
-              placeholder="ISRC code"
+              placeholder='ISRC code'
             />
           </FormControl>
-          <FormControl id="upc">
+          <FormControl id='upc'>
             <FormLabel>UPC</FormLabel>
             <Input 
               value={upc}
               onChange={(e) => setUpc(e.target.value)}
-              placeholder="UPC code"
+              placeholder='UPC code'
             />
           </FormControl>
         </Stack>
         <Stack direction={{ base: 'column', md: 'row' }} spacing={4} mt={4}>
-          <FormControl id="tag">
+          <FormControl id='tag'>
             <FormLabel>Tag</FormLabel>
             <Select 
               value={tag}
               onChange={(e) => setTag(e.target.value)}
             >
-              <option value="">None</option>
-              <option value="hipster">Hipster</option>
-              <option value="new">New</option>
+              <option value=''>None</option>
+              <option value='hipster'>Hipster</option>
+              <option value='new'>New</option>
             </Select>
           </FormControl>
         </Stack>
-        <Button mt={4} type="submit">Search</Button>
+        <Button mt={4} type='submit'>Search</Button>
       </form>
 
-      {isLoading && <Spinner size="xl" mt={4} />}
+      {isLoading && <Spinner size='xl' mt={4} />}
 
       {error && (
-        <Alert status="error" mt={4}>
+        <Alert status='error' mt={4}>
           <AlertIcon />
           Error: {error.message}
         </Alert>
@@ -261,83 +267,54 @@ export const FindMusicRoute = () => {
         <Box mt={4}>
           {type.includes('album') && data.albums?.items.length > 0 && (
             <>
-              <Heading size="md" mb={2}>Albums</Heading>
-              <Button onClick={() => setExpandAlbums(!expandAlbums)} mb={4}>
-                {expandAlbums ? 'Minimize Albums' : 'Expand Albums'}
+              <Heading size='md' mb={2}>Albums</Heading>
+              <Button onClick={() => toggleExpandCategory('album')} mb={4}>
+                {expandedCategories.album ? 'Minimize albums' : 'Expand albums'}
               </Button>
-              <Collapse in={expandAlbums}>
+              <Collapse in={expandedCategories.album}>
                 <SimpleGrid columns={breakpointColumns} spacing={4} mb={4}>
                   {renderItems(data.albums.items, 'album')}
                 </SimpleGrid>
               </Collapse>
-              {expandAlbums && <Button onClick={() => setExpandAlbums(!expandAlbums)} mb={4}>
-                {expandAlbums ? 'Minimize Albums' : 'Expand Albums'}
+              {expandedCategories.album && <Button onClick={() => toggleExpandCategory('album')} mb={4}>
+                {expandedCategories.album ? 'Minimize albums' : 'Expand albums'}
               </Button>}
             </>
           )}
           {type.includes('artist') && data.artists?.items.length > 0 && (
             <>
-              <Heading size="md" mb={2}>Artists</Heading>
-              <Button onClick={() => setExpandArtists(!expandArtists)} mb={4}>
-                {expandArtists ? 'Minimize Artists' : 'Expand Artists'}
+              <Heading size='md' mb={2}>Artists</Heading>
+              <Button onClick={() => toggleExpandCategory('artist')} mb={4}>
+                {expandedCategories.artist ? 'Minimize Artists' : 'Expand Artists'}
               </Button>
-              <Collapse in={expandArtists}>
+              <Collapse in={expandedCategories.artist}>
                 <SimpleGrid columns={breakpointColumns} spacing={4} mb={4}>
                   {renderItems(data.artists.items, 'artist')}
                 </SimpleGrid>
               </Collapse>
-              {expandArtists && <Button onClick={() => setExpandArtists(!expandArtists)} mb={4}>
-                {expandArtists ? 'Minimize Artists' : 'Expand Artists'}
+              {expandedCategories.artist && <Button onClick={() => toggleExpandCategory('artist')} mb={4}>
+                {expandedCategories.artist ? 'Minimize Artists' : 'Expand Artists'}
               </Button>}
             </>
           )}
           {type.includes('track') && data.tracks?.items.length > 0 && (
             <>
-              <Heading size="md" mb={2}>Tracks</Heading>
-              <Button onClick={() => setExpandTracks(!expandTracks)} mb={4}>
-                {expandTracks ? 'Minimize Tracks' : 'Expand Tracks'}
+              <Heading size='md' mb={2}>Tracks</Heading>
+              <Button onClick={() => toggleExpandCategory('track')} mb={4}>
+                {expandedCategories.track ? 'Minimize Tracks' : 'Expand Tracks'}
               </Button>
-              <Collapse in={expandTracks}>
+              <Collapse in={expandedCategories.track}>
                 <SimpleGrid columns={breakpointColumns} spacing={4} mb={4}>
                   {renderItems(data.tracks.items, 'track')}
                 </SimpleGrid>
               </Collapse>
-              {expandTracks && <Button onClick={() => setExpandTracks(!expandTracks)} mb={4}>
-                {expandTracks ? 'Minimize Tracks' : 'Expand Tracks'}
+              {expandedCategories.track && <Button onClick={() => toggleExpandCategory('track')} mb={4}>
+                {expandedCategories.track ? 'Minimize tracks' : 'Expand tracks'}
               </Button>}
             </>
           )}
         </Box>
       )}
-
-      {/* {data && (
-        <Box mt={4}>
-          {type.includes('album') && data.albums?.items.length > 0 && (
-            <>
-              <Heading size="md" mb={4}>Albums</Heading>
-              <SimpleGrid columns={breakpointColumns} spacing={4} mb={4}>
-                {renderItems(data.albums.items, 'album')}
-              </SimpleGrid>
-            </>
-          )}
-          {type.includes('artist') && data.artists?.items.length > 0 && (
-            <>
-              <Heading size="md" mb={4}>Artists</Heading>
-              <SimpleGrid columns={breakpointColumns} spacing={4} mb={4}>
-                {renderItems(data.artists.items, 'artist')}
-              </SimpleGrid>
-            </>
-          )}
-          {type.includes('track') && data.tracks?.items.length > 0 && (
-            <>
-              <Heading size="md" mb={4}>Tracks</Heading>
-              <SimpleGrid columns={breakpointColumns} spacing={4} mb={4}>
-                {renderItems(data.tracks.items, 'track')}
-              </SimpleGrid>
-            </>
-          )}
-        </Box>
-      )} */}
     </Box>
   );
 };
